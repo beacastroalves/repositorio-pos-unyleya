9d93a399f30921d46ad161de112785c8184fa726
beacastroalves <beacastro1008@gmail.com>
2025-07-15 15:38:06 +0100

    Masterclass complementar de Vuejs

---
+++ unyleya-vuejs/complementar/aulao-vue/src/components/PrimeiroComponente.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Primeiro Componente</h1>
+  </div>
+</template>
+
+<style scoped>
+div {
+  background-color: #482222;
+  padding: 16px;
+  margin: 32px;
+  border-radius: 4px;
+  text-align: center;
+}
+
+div:hover {
+  background-color: #653030;
+}
+
+h1 {
+  color: white;
+  margin-bottom: 8px;
+  margin: 0;
+}
+</style>
---
+++ unyleya-vuejs/complementar/aulao-vue/src/components/Slots.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="componente">
+    <h1>Componente com Slots</h1>
+    <button class="btn-slot">
+      <slot name="icone"></slot>
+      <slot name="texto">Botão</slot>
+    </button>
+  </div>
+</template>
+
+
+<style scoped>
+.btn-slot {
+  display: flex;
+  text-align: center;
+  align-items: flex-end;
+  gap: 4px;
+  margin: 0 auto;
+}
+</style>
