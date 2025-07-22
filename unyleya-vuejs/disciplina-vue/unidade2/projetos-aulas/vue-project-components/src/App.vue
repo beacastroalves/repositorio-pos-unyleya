6abebfd0283f13c887eda4282c2629f0c2c7034f
beacastroalves <beacastro1008@gmail.com>
2025-07-22 09:30:34 +0100

    introdução e criação do primeiro componente

---
+++ unyleya-vuejs/disciplina-vue/unidade2/projetos-aulas/vue-project-components/src/App.vue
@@ -0,0 +1,59 @@
+<!-- Composition API (Modo mais usado hoje em dia) -->
+ <script setup>
+import NumeroAleatorio from './components/NumeroAleatorio.vue';
+</script>
+
+<!-- Options API (Modo usado antigamente)
+<script>
+import NumeroAleatorio from './components/NumeroAleatorio.vue';
+export default {
+  components: {
+    NumeroAleatorio
+  }
+}
+</script>
+-->
+
+<template>
+  <header>
+    <h1>App</h1>
+  </header>
+
+  <main>
+    <NumeroAleatorio />
+    <NumeroAleatorio />
+    <NumeroAleatorio />
+    <NumeroAleatorio />
+    <NumeroAleatorio />
+    <NumeroAleatorio />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ unyleya-vuejs/disciplina-vue/unidade2/projetos-aulas/vue-project-components/src/components/NumeroAleatorio.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from 'vue';
+
+const numAleatorio = ref(0);
+const gerarNumAleatorio = () => {
+  const numBase = Math.random();
+  const numFloat = numBase * 10;
+  const numInt = Math.floor(numFloat);
+  numAleatorio.value = numInt;
+}
+</script>
+
+<template>
+  <section>
+    <p>{{ numAleatorio }}</p>
+    <button @click="gerarNumAleatorio">Lançar</button>
+  </section>
+</template>
+
+<style scoped>
+section {
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid lightgray;
+  border-top: 4px solid #fed766;
+  border-bottom: 4px solid #fed766;
+  border-radius: 4px;
+  box-shadow: 0 0 4px lightgray;
+}
+
+section > p {
+  font-size: 2rem;
+}
+
+section > button {
+  border: none;
+  background-color: #0e9aa7;
+  color: white;
+  padding: 4px;
+  border-radius: 4px;
+  transition-duration: 300ms;
+}
+
+section > button:hover {
+  background-color: #4a4e4d;
+  color: white;
+  box-shadow: 0 0 4px lightgray;
+}
+</style>
