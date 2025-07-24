3f51a2df208b68419b32a5f7740aac1d4624caf4
beacastroalves <beacastro1008@gmail.com>
2025-07-24 19:09:16 +0100

    componentes com slots - pt2 final

--- unyleya-vuejs/disciplina-vue/unidade2/projetos-aulas/vue-project-components/src/components/Galeria.vue
+++ unyleya-vuejs/disciplina-vue/unidade2/projetos-aulas/vue-project-components/src/components/Galeria.vue
@@ -1,28 +1,14 @@
 <script setup>
 import { computed, ref } from 'vue';
-import CardEvento from './CardEvento.vue';
-// import CardProduto from './CardProduto.vue';
 
-const { itens } = defineProps([
-  'titulo', ' descricao', 'itens'
+const { quant, numPorPagina } = defineProps([
+  'titulo', ' descricao', 'pagina', 'quant', 'numPorPagina'
 ]);
 
-const numPorPagina = 2;
-const pagina = ref(1);
-const avancarPagina = () => pagina.value++;
-const retrocederPagina = () => pagina.value--;
 const quantPagina = computed(() => {
-  return Math.ceil(itens.length / numPorPagina)
+  return Math.ceil(quant / numPorPagina)
 });
 
-const itensPaginados = computed(() => {
-  const pag = pagina.value - 1;
-  return itens.filter((_, index) => {
-    const limiteMin = numPorPagina * pag;
-    const limiteMax = limiteMin + numPorPagina;
-    return index >= limiteMin && limiteMax > index;
-  })
-});
 </script>
 
 <template>
@@ -30,12 +16,10 @@ const itensPaginados = computed(() => {
     <h2>Galeria de {{ titulo }}</h2>
     <p>{{ descricao }}</p>
 
-    <!-- Galeria Generica Entra Aqui -->
-     <!-- <CardProduto v-for="item in itensPaginados" :produto="item" /> -->
-     <CardEvento v-for="item in itensPaginados" :evento="item" />
+     <slot></slot>
 
-    <button @click="retrocederPagina">Retroceder</button>
+    <button @click="$emit('retroceder-pagina')">Retroceder</button>
     {{ pagina }} / {{ quantPagina }}
-    <button @click="avancarPagina">Avançar</button>
+    <button @click="$emit('avancar-pagina')">Avançar</button>
   </section>
 </template>
