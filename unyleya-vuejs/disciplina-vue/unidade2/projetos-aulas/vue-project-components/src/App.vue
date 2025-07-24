84670f56e0f2ff2c2c7a039529aae8cc8d265d88
beacastroalves <beacastro1008@gmail.com>
2025-07-24 21:48:05 +0100

    componentes slot nomeados pt1 intro

--- unyleya-vuejs/disciplina-vue/unidade2/projetos-aulas/vue-project-components/src/App.vue
+++ unyleya-vuejs/disciplina-vue/unidade2/projetos-aulas/vue-project-components/src/App.vue
@@ -1,43 +1,7 @@
 <script setup>
-import { computed, ref } from 'vue';
-import Galeria from './components/Galeria.vue';
-import CardEvento from './components/CardEvento.vue';
+import LayoutPage from './components/LayoutPage.vue';
 
-// const itens = [
-//   { nome: "Computador", descricao: "Computador Gamer", preco: 9_000.00 },
-//   { nome: "Mouse", descricao: "Mouse Gamer", preco: 90.00 },
-//   { nome: "Teclado", descricao: "Teclado Gamer", preco: 180.00 },
-//   { nome: "Monitor", descricao: "Monitor Gamer", preco: 1_000.00 },
-// ]
 
-const itens = [
-  { nome: "Musica", descricao: "Musicas de Sucesso", data: "05/01/2026", local: "Casa de Eventos" },
-  { nome: "Cinema", descricao: "Cinema de Sucesso", data: "02/01/2026", local: "Cinema Local" },
-  { nome: "Teatro", descricao: "Teatro de Sucesso", data: "08/01/2026", local: "Teatro Municipal" },
-  { nome: "Orquestra", descricao: "Orquestra de Sucesso", data: "21/01/2026", local: "Praça da Cidade" },
-]
-
-const numPorPagina = 2;
-const pagina = ref(1);
-const avancarPagina = () => {
-  if (pagina.value < (itens.length / numPorPagina)) {
-    pagina.value++
-  }
-};
-const retrocederPagina = () => {
-  if (pagina.value >= (itens.length / numPorPagina)) {
-    pagina.value--
-  }
-};
-
-const itensPaginados = computed(() => {
-  const pag = pagina.value - 1;
-  return itens.filter((_, index) => {
-    const limiteMin = numPorPagina * pag
-    const limiteMax = limiteMin + numPorPagina
-    return index >= limiteMin && limiteMax > index
-  })
-});
 </script>
 
 <template>
@@ -46,23 +10,21 @@ const itensPaginados = computed(() => {
   </header>
 
   <main>
-    <!-- <Galeria titulo="Produtos" descricao="Produtos Eletronicos" :itens="produtos" /> -->
-    <Galeria
-      titulo="Eventos"
-      descricao="Eventos na Cidade"
-      :quant="itens.length"
-      :numPorPagina="numPorPagina"
-      :pagina="pagina"
-      @avancar-pagina="avancarPagina"
-      @retroceder-pagina="retrocederPagina"
-      >
+    <LayoutPage>
+      <template v-slot:header>
+        <h1>Cabeçalho</h1>
+      </template>
 
-      <CardEvento
-        v-for="item in itensPaginados"
-        :evento="item"
-        />
+      <!-- Colocar Default ou apenas colocar qualquer conteudo fora do template vai para o espaço do slot que nao possui nome -->
+      <!-- <template #default>
+        <h2>Conteudo Principal</h2>
+      </template> -->
 
-    </Galeria>
+      <!-- Pode existir valor/conteudo dentro dos templates la nos slots, vai ser um valor padrao, e aqui se for colocado vai ser sobreescrever -->
+      <!-- <template v-slot:footer>
+        <p>Rodape</p>
+      </template> -->
+    </LayoutPage>
   </main>
 </template>
 
---
+++ unyleya-vuejs/disciplina-vue/unidade2/projetos-aulas/vue-project-components/src/components/LayoutPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <slot name="header">
+      <h1>Cabeçalho da Página</h1>
+    </slot>
+  </header>
+  <main>
+    <slot>
+      <h2>Conteudo da Página</h2>
+    </slot>
+  </main>
+  <footer>
+    <slot name="footer">
+      <h3>Rodape da Página</h3>
+    </slot>
+  </footer>
+</template>
