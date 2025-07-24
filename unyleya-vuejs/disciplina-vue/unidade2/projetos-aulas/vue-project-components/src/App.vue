5c384c02f362529288b291f4464047d72551606b
beacastroalves <beacastro1008@gmail.com>
2025-07-24 10:52:28 +0100

    compartilhamento de dados de filho para pai

--- unyleya-vuejs/disciplina-vue/unidade2/projetos-aulas/vue-project-components/src/App.vue
+++ unyleya-vuejs/disciplina-vue/unidade2/projetos-aulas/vue-project-components/src/App.vue
@@ -1,18 +1,14 @@
 <script setup>
-import { ref, reactive } from 'vue';
 import Menu from './components/Menu.vue';
 import Topico from './components/Topico.vue';
 import Respostas from './components/Respostas.vue';
+import FormResposta from './components/FormResposta.vue';
+import { reactive } from 'vue';
 
-const novaResposta = ref(null);
 const respostas = reactive([])
 
-const enviarResp = () => {
-  const conteudo = novaResposta.value;
-  const data = new Date().toLocaleString("pt-BR").substring(0, 10);
-  const autor = "Beatriz Castro";
-  novaResposta.value = null;
-  respostas.push({conteudo, data, autor});
+const addResposta = (resposta) => {
+  respostas.push(resposta);
 }
 </script>
 
@@ -25,10 +21,7 @@ const enviarResp = () => {
     <Topico />
     <section>
       <Respostas :respostas="respostas"/>
-      <div class="resposta__form">
-        <textarea v-model="novaResposta"></textarea>
-        <button @click="enviarResp">Enviar</button>
-      </div>
+      <FormResposta @eventAddResposta='addResposta' />
     </section>
   </main>
 </template>
@@ -41,16 +34,6 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
-}
-
-.resposta__form {
-  border-top: 1px solid lightgray;
-  padding: 10px;
-  display: flex;
-}
-
-.resposta__form > textarea {
-  flex-grow: 1;
 }
 
 @media (min-width: 1024px) {
---
+++ unyleya-vuejs/disciplina-vue/unidade2/projetos-aulas/vue-project-components/src/components/FormResposta.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref, reactive } from 'vue';
+
+const emit = defineEmits(['eventAddResposta']);
+const novaResposta = ref(null);
+
+const enviarResp = () => {
+  const conteudo = novaResposta.value;
+  const data = new Date().toLocaleString("pt-BR").substring(0, 10);
+  const autor = "Beatriz Castro";
+
+  novaResposta.value = null;
+  emit('eventAddResposta', { conteudo, data, autor });
+}
+</script>
+
+<template>
+  <main>
+      <div class="resposta__form">
+        <textarea v-model="novaResposta"></textarea>
+        <button @click='enviarResp'>Enviar</button>
+      </div>
+  </main>
+</template>
+
+
+<style scoped>
+.resposta__form {
+  border-top: 1px solid lightgray;
+  padding: 10px;
+  display: flex;
+}
+
+.resposta__form > textarea {
+  flex-grow: 1;
+}
+</style>
