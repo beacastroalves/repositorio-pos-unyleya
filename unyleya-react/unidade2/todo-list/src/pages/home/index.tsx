deab47abacb35efaeb081c7439b56d9912eb9ffa
beacastroalves <beacastro1008@gmail.com>
2025-08-21 18:40:26 +0100

    formularios usando useState

--- unyleya-react/unidade2/todo-list/src/pages/home/index.tsx
+++ unyleya-react/unidade2/todo-list/src/pages/home/index.tsx
@@ -1,10 +1,35 @@
+import { useState } from 'react';
+import './style.css';
+
 const Home = () => {
 
-
+  const [titulo, setTitulo] = useState("");
+  const [categoria, setcategoria] = useState("");
+  const [data, setData] = useState("");
+  const [descricao, setDescricao] = useState("");
 
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className='container-home'>
+      <div className='container-form'>
+        <form>
+          <h2>Cadastrar Tarefa</h2>
+          <input type="text" placeholder="Titulo" value={titulo} onChange={(event) => setTitulo(event.target.value)} />
+
+          <select value={categoria} onChange={(event) => setcategoria(event.target.value)}>
+            <option value="">Selecione a categoria</option>
+            <option value="trabalho">Trabalho</option>
+            <option value="estudo" >Estudo</option>
+          </select>
+
+          <input type="date" placeholder="Data" value={data} onChange={(event) => setData(event.target.value)} />
+
+          <textarea placeholder="Descrição da tarefa" value={descricao} onChange={(event) => setDescricao(event.target.value)}></textarea>
+          <button>Salvar</button>
+        </form>
+      </div>
+      <div className='container-tarefas'>
+        <h1>Minhas Tarefas</h1>
+      </div>
     </div>
   );
 };
--- unyleya-react/unidade2/todo-list/src/pages/login/index.tsx
+++ unyleya-react/unidade2/todo-list/src/pages/login/index.tsx
@@ -1,19 +1,25 @@
-import { useState } from 'react';
 import './style.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
   const [name, setName] = useState("");
-  console.log(name);
+
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/home");
+  };
 
   return (
     <div className='container-login'>
       <div className='card-login'>
         <h1>Informe seu nome</h1>
 
-        <input onChange={(evento) => setName(evento.target.value)} type="text" placeholder='Exemplo: Beatriz Castro' />
+        <input value={name} onChange={(evento) => setName(evento.target.value)} type="text" placeholder='Exemplo: Beatriz Castro' />
 
-        <button>Entrar</button>
+        <button onClick={handleNavigate}>Entrar</button>
       </div>
     </div>
   );
