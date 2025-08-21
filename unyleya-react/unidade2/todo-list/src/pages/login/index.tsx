31c579adc9d38dfbdbd3127dc7d8c29e1cecbba6
beacastroalves <beacastro1008@gmail.com>
2025-08-21 12:53:08 +0100

    criando o componente do login

--- unyleya-react/unidade2/todo-list/src/pages/login/index.tsx
+++ unyleya-react/unidade2/todo-list/src/pages/login/index.tsx
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+import './style.css';
+
 const Login = () => {
+
+  const [name, setName] = useState("");
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className='container-login'>
+      <div className='card-login'>
+        <h1>Informe seu nome</h1>
+
+        <input onChange={(evento) => setName(evento.target.value)} type="text" placeholder='Exemplo: Beatriz Castro' />
+
+        <button>Entrar</button>
+      </div>
     </div>
   );
 };
