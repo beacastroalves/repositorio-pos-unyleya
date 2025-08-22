be0427b34cbf19ae794ee3e25ea6dd683f7176d9
beacastroalves <beacastro1008@gmail.com>
2025-08-22 14:04:17 +0100

    Listando itens com função Map() + css e estilo

--- unyleya-react/unidade2/todo-list/src/pages/home/index.tsx
+++ unyleya-react/unidade2/todo-list/src/pages/home/index.tsx
@@ -1,5 +1,26 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './style.css';
+
+const tasks = [
+  {
+    title: "Tarefa 1",
+    category: "Trabalho",
+    description: "Descrição teste 1",
+    date: "22-08-2025"
+  },
+  {
+    title: "Tarefa 2",
+    category: "Trabalho",
+    description: "Descrição teste 2",
+    date: "22-08-2025"
+  },
+  {
+    title: "Tarefa 3",
+    category: "Estudo",
+    description: "Descrição teste 3",
+    date: "22-08-2025"
+  },
+]
 
 const Home = () => {
 
@@ -8,10 +29,19 @@ const Home = () => {
   const [data, setData] = useState("");
   const [descricao, setDescricao] = useState("");
 
+  const [tarefas, setTarefas] = useState(tasks);
+  console.log(setTarefas);
+
+  const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log({ titulo, categoria, data, descricao});
+  };
+
   return (
     <div className='container-home'>
       <div className='container-form'>
-        <form>
+        <form onSubmit={submitForm}>
           <h2>Cadastrar Tarefa</h2>
           <input type="text" placeholder="Titulo" value={titulo} onChange={(event) => setTitulo(event.target.value)} />
 
@@ -24,11 +54,32 @@ const Home = () => {
           <input type="date" placeholder="Data" value={data} onChange={(event) => setData(event.target.value)} />
 
           <textarea placeholder="Descrição da tarefa" value={descricao} onChange={(event) => setDescricao(event.target.value)}></textarea>
-          <button>Salvar</button>
+          <button type='submit'>Salvar</button>
         </form>
       </div>
       <div className='container-tarefas'>
-        <h1>Minhas Tarefas</h1>
+        <h2>Minhas Tarefas</h2>
+        <ul>
+          {
+            tarefas.map((tarefa) => (
+                <li>
+                  <div className='itens-left'>
+                    <h4>{tarefa.title}</h4>
+                    <p>{tarefa.category}</p>
+                    <p>{tarefa.description}</p>
+                  </div>
+                  <div className='itens-right'>
+                    <p>{tarefa.date}</p>
+                    <div className='tarefas-buttons'>
+                      <button>Editar</button>
+                      <button>Apagar</button>
+                    </div>
+                  </div>
+                </li>
+              )
+            )
+          }
+        </ul>
       </div>
     </div>
   );
