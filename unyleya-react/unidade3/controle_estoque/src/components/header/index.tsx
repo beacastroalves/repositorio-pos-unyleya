dc98a90f31956176e2fe02c839cf63f414e38afc
beacastroalves <beacastro1008@gmail.com>
2025-08-25 17:26:25 +0100

    criando components iniciais

---
+++ unyleya-react/unidade3/controle_estoque/src/components/header/index.tsx
@@ -0,0 +1,16 @@
+import './style.css';
+
+const Header = () => {
+  return (
+    <div className='container-header'>
+      <h1>UnyProducts</h1>
+      <ul>
+        <li>Products</li>
+        <li>Quem somos</li>
+        <li>Sair</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
--- unyleya-react/unidade3/controle_estoque/src/pages/home/index.tsx
+++ unyleya-react/unidade3/controle_estoque/src/pages/home/index.tsx
@@ -1,8 +1,19 @@
+import Header from "../../components/header";
+import './style.css';
+
 const Home = () => {
   return (
-    <>
-      <h1>Hello world - Home</h1>
-    </>
+    <div className="container-home">
+      <Header />
+      <h2>Produtos</h2>
+
+      <div className="card">
+        <h3>Echo Dot (8ª Geração)</h3>
+        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.meliuz.com.br%2Fblog%2Fwp-content%2Fuploads%2F2023%2F05%2F7-1.jpg&f=1&nofb=1&ipt=becd165f22745b5a9c7e3592cf0ed062f36515e848bae193de11a9627d090793" alt="" />
+        <p>Amazon</p>
+        <p>R$ 700.99</p>
+      </div>
+    </div>
   );
 };
 
